e21b7dcb4c1181dc8b3f79d022b528e71447985c
VIN124-VINNI <vineelareddy925@gmail.com>
2024-10-25 08:59:00 +0530

    Add files via upload

---
+++ lopps.js
@@ -0,0 +1,37 @@
+for (var a=1;a<7;a++){ //post increment//
+    console.log(a)
+    }
+    for (var a=10;a>2;a--){ //post increment//
+      console.log(a)
+      }
+      
+    for (var a=2;a<=7;a++){
+      console.log(a)
+    }
+    for (var a=10;a>5;a--){
+    }
+      console.log(a)
+    
+    var a=10;
+    while (a>=5){
+      console.log(a)
+      a--
+    }
+    var a=5;
+    while (a<=10){
+      console.log(a);
+      a++
+    }
+    var a=10;
+    do {
+      a--
+      console.log(a)
+    }
+    while(a>5)
+    var a=3;
+    do {
+      a++
+      console.log(a)
+    }
+    while(a<12);
+    
